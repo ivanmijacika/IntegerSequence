c431d001f960dcae20aa1cc74f4a1ddba4b0dece
ivanmijacika <imijacika20@stuy.edu>
2020-12-05 20:45:40 -0500

    Tester added with everything working

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester{
+  public static void main(String[] args){
+    IntegerSequence s = new Range(1,10);
+    while(s.hasNext()){
+      System.out.print(s.next());
+      if (s.hasNext()) System.out.print(", ");
+    }
+  }
+}
