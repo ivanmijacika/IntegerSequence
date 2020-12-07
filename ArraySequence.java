6ef4d5c378756c228630ef6c3137140e361b8538
ivanmijacika <imijacika20@stuy.edu>
2020-12-07 12:54:37 -0500

    copying part of constructor changed

--- ArraySequence.java
+++ ArraySequence.java
@@ -2,11 +2,13 @@ import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
 
   int currentIndex;
-  int []data;
+  int[] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int[] other){
-    data = other;
+    for (int i=0; i<other.length; i++){
+      data[i] = other[i];
+    }
     currentIndex = 0;
   }
 
